2d81c6c3100247479520dff38a1a0c01e2a664f2
Shadow Cun <vinthony@gmail.com>
2023-04-17 21:16:00 +0800

    Update download_models.sh

--- scripts/download_models.sh
+++ scripts/download_models.sh
@@ -14,7 +14,7 @@ wget -nc https://github.com/Winfredy/SadTalker/releases/download/v0.0.2/hub.zip 
 unzip -n ./checkpoints/hub.zip -d ./checkpoints/
 unzip -n ./checkpoints/BFM_Fitting.zip -d ./checkpoints/
 
-mkdir ./gfpgan/weights
+mkdir -p ./gfpgan/weights
 wget -nc https://github.com/xinntao/facexlib/releases/download/v0.1.0/alignment_WFLW_4HG.pth -O ./gfpgan/weights/alignment_WFLW_4HG.pth 
 wget -nc https://github.com/xinntao/facexlib/releases/download/v0.1.0/detection_Resnet50_Final.pth -O ./gfpgan/weights/detection_Resnet50_Final.pth 
 wget -nc https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth -O ./gfpgan/weights/GFPGANv1.4.pth 
