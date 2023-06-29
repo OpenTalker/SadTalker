9af5b140ecefd7e7b561321747c12a41e3698f2b
shadowcun <shadowcun@tencent.com>
2023-06-29 21:25:08 +0800

    fixed: cannot running on cpu mode caused by facexlib

--- src/face3d/extract_kp_videos_safe.py
+++ src/face3d/extract_kp_videos_safe.py
@@ -8,9 +8,27 @@ from PIL import Image
 import torch
 from tqdm import tqdm
 from itertools import cycle
-from facexlib.alignment import init_alignment_model, landmark_98_to_68
-from facexlib.detection import init_detection_model
 from torch.multiprocessing import Pool, Process, set_start_method
+
+from facexlib.alignment import landmark_98_to_68
+from facexlib.detection import init_detection_model
+
+from facexlib.utils import load_file_from_url
+from facexlib.alignment.awing_arch import FAN
+
+def init_alignment_model(model_name, half=False, device='cuda', model_rootpath=None):
+    if model_name == 'awing_fan':
+        model = FAN(num_modules=4, num_landmarks=98, device=device)
+        model_url = 'https://github.com/xinntao/facexlib/releases/download/v0.1.0/alignment_WFLW_4HG.pth'
+    else:
+        raise NotImplementedError(f'{model_name} is not implemented.')
+
+    model_path = load_file_from_url(
+        url=model_url, model_dir='facexlib/weights', progress=True, file_name=None, save_dir=model_rootpath)
+    model.load_state_dict(torch.load(model_path, map_location=device)['state_dict'], strict=True)
+    model.eval()
+    model = model.to(device)
+    return model
 
 
 class KeypointExtractor():
