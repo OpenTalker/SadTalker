b24f743cf5c98c9e706d1364bdb14cd8b0d5b18c
Simmer1234569 <85909485+Simmer1234569@users.noreply.github.com>
2023-08-16 07:49:45 -0700

    Improved speed my 300% on face renderer
    Credit to someone in the issues tab for helping out with this but I took their code a little further and the speed is noticable.

--- src/utils/paste_pic.py
+++ src/utils/paste_pic.py
@@ -1,29 +1,30 @@
-import cv2, os
+import cv2
+import os
 import numpy as np
 from tqdm import tqdm
 import uuid
-
-from src.utils.videoio import save_video_with_watermark 
+import concurrent.futures
+from src.utils.videoio import save_video_with_watermark
 
 def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, extended_crop=False):
 
     if not os.path.isfile(pic_path):
         raise ValueError('pic_path must be a valid path to video/image file')
     elif pic_path.split('.')[-1] in ['jpg', 'png', 'jpeg']:
-        # loader for first frame
+        # Loader for first frame
         full_img = cv2.imread(pic_path)
     else:
-        # loader for videos
+        # Loader for videos
         video_stream = cv2.VideoCapture(pic_path)
-        fps = video_stream.get(cv2.CAP_PROP_FPS)
-        full_frames = [] 
+        pic_fps = video_stream.get(cv2.CAP_PROP_FPS)
         while 1:
             still_reading, frame = video_stream.read()
             if not still_reading:
                 video_stream.release()
                 break 
-            break 
-        full_img = frame
+            full_img = frame  # Use the first frame as full_img
+            break  # Only process the first frame
+
     frame_h = full_img.shape[0]
     frame_w = full_img.shape[1]
 
@@ -45,9 +46,7 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
         clx, cly, crx, cry = crop_info[1]
         lx, ly, rx, ry = crop_info[2]
         lx, ly, rx, ry = int(lx), int(ly), int(rx), int(ry)
-        # oy1, oy2, ox1, ox2 = cly+ly, cly+ry, clx+lx, clx+rx
-        # oy1, oy2, ox1, ox2 = cly+ly, cly+ry, clx+lx, clx+rx
-
+        
         if extended_crop:
             oy1, oy2, ox1, ox2 = cly, cry, clx, crx
         else:
@@ -55,15 +54,26 @@ def paste_pic(video_path, pic_path, crop_info, new_audio_path, full_video_path, 
 
     tmp_path = str(uuid.uuid4())+'.mp4'
     out_tmp = cv2.VideoWriter(tmp_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (frame_w, frame_h))
-    for crop_frame in tqdm(crop_frames, 'seamlessClone:'):
-        p = cv2.resize(crop_frame.astype(np.uint8), (ox2-ox1, oy2 - oy1)) 
 
+    def process_image(crop_frame):
+        p = cv2.resize(crop_frame.astype(np.uint8), (ox2-ox1, oy2 - oy1))
         mask = 255*np.ones(p.shape, p.dtype)
         location = ((ox1+ox2) // 2, (oy1+oy2) // 2)
         gen_img = cv2.seamlessClone(p, full_img, mask, location, cv2.NORMAL_CLONE)
-        out_tmp.write(gen_img)
+        return gen_img
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        processed_frames = []
+        for gen_img in tqdm(executor.map(process_image, crop_frames), total=len(crop_frames), desc='seamlessClone:'):
+            processed_frames.append(gen_img)
+
+    for frame in processed_frames:
+        out_tmp.write(frame)
 
     out_tmp.release()
+
+    save_video_with_watermark(tmp_path, new_audio_path, full_video_path, watermark=False)
+    os.remove(tmp_path)
 
     save_video_with_watermark(tmp_path, new_audio_path, full_video_path, watermark=False)
     os.remove(tmp_path)
