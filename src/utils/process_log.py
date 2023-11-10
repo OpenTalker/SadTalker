c0841f660d8b3f88d1aad12ed735679a61f9fa3c
Anlim <anlimweizhang@gmail.com>
2023-11-10 23:30:23 +0800

    feat: log file

--- src/utils/process_log.py
+++ src/utils/process_log.py
@@ -1,7 +1,8 @@
 import csv
+import os
 
 
 def record_process_log(func_name, tag, duration, relate_file=None):
-    with open("process_log.csv", "a+") as f:
+    with open(os.path.join(os.getcwd(), "process_log.csv"), "a+") as f:
         writer = csv.writer(f)
         writer.writerow([func_name, tag, duration, relate_file])
