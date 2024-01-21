6cde5b972ce64a51f5cf4c773814a0788b701d89
叶璨铭 <12011404@mail.sustech.edu.cn>
2024-01-22 04:18:25 +0800

    feat(luna ai connect): async process 2 request at onece

--- luna_sad_direct.py
+++ luna_sad_direct.py
@@ -10,10 +10,24 @@ import pygame
 import hashlib
 from fastapi import FastAPI
 import uvicorn
+from pathlib import Path
+
 
 
 face_path = r"E:/AsciiStandardPath/InnovativeExploration/CFAR/girl_face.jpeg"
+待机动画 = r'E:\AsciiStandardPath\InnovativeExploration\CFAR\SadTalker\data\girl_face待机动画1.mp4'
+music_directory = Path(r'C:\Users\YeCanming\Music\ ').resolve()
+直播室背景音乐 = [x.as_posix() for x in music_directory.glob('*.mp3')]
+# 直播室背景音乐 = [f"{music_directory.as_posix()}/【P1】AuntTagonist(即通辽进行曲)_20240122_01022800.mp3", 
+#            ]
+import random
+random.shuffle(直播室背景音乐)
+
 crop_size = 256 # 不能是str
+import tempfile
+import shutil
+
+temp_dir = tempfile.TemporaryDirectory()
 
 
 from src.gradio_demo import SadTalker
@@ -26,9 +40,12 @@ sad_talker = SadTalker(checkpoint_path="checkpoints", config_path="src/config", 
 #                 serialize=True)
 
 args = dict(
-        source_image=face_path, 
+        source_image=None, 
         driven_audio=None, preprocess='crop', 
-        still_mode=True,  use_enhancer=False, batch_size=2, size=crop_size, 
+        still_mode=True,  use_enhancer=False, 
+        # batch_size=2, 
+        batch_size=8, 
+        size=crop_size, 
         pose_style = 0, 
         # exp_scale=1.0, 
         # use_ref_video = False,
@@ -38,23 +55,36 @@ args = dict(
         # length_of_audio = 0, use_blink=True,
         # result_dir='./results/'
 )
-import tempfile
-import shutil
-
-temp_dir = tempfile.TemporaryDirectory()
+def make_copy(path):
+    try:
+        shutil.copy(path, temp_dir.name)
+    except shutil.SameFileError:
+        pass
+    path = os.path.join(temp_dir.name, os.path.basename(path))
+    return path
+# async def generate_video(audio_path, output_path=None):
 def generate_video(audio_path, output_path=None):
+    global face_path
+    local_face_path = make_copy(face_path)
+    audio_path = make_copy(audio_path)
+    
+    if output_path is not None:
+        Path(output_path).parent.mkdir(parents=True, exist_ok=True)
     args.update(
         dict(
+            source_image=local_face_path,
             driven_audio=audio_path,
             result_dir=temp_dir.name
         )
     )
     result = sad_talker.test(**args)
+    # result = await sad_talker.test(**args)
+    result_path = result
     # result = client.predict(
     #         **args, driven_audio=audio_path, 
     #         api_name="/test"
     #     )
-    result_path = result['video']
+    # result_path = result['video']
     
     if output_path is not None:
         shutil.move(result_path, output_path)
@@ -88,7 +118,6 @@ def convert_mp3_to_wav(input_file, output_file):
 
         # 读取音频文件
         # 不是Python的bug，是缺了目录, 需要创建一个audio文件夹
-        from pathlib import Path
 
         Path(input_file).parent.mkdir(parents=True, exist_ok=True)
         Path(output_file).parent.mkdir(parents=True, exist_ok=True)
@@ -105,18 +134,108 @@ def convert_mp3_to_wav(input_file, output_file):
         raise e
         print(f"Error: {e}")
 
-def play_audio(audio_file):
-    pygame.mixer.init()
-    pygame.mixer.music.load(audio_file)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
+# https://www.pygame.org/docs/ref/mixer.html
+pygame.mixer.init()
+pygame.mixer.set_num_channels(8)
+def play_audio(audio_file, volume=1.0, loops=0):
+    # The loops argument controls how many times the sample will be repeated after being played the first time.
+    # 注意loops是额外的次数，而不是总共的次数。所以0表示播放1次。-1是特殊情况，无限循环。
+    sound = pygame.mixer.Sound(audio_file)
+    sound.set_volume(volume)
+    sound.play(loops=loops)
+    while pygame.mixer.get_busy():
         pygame.time.Clock().tick(10)
-    pygame.mixer.music.stop()
+    
+    
+def play_video():
+    """不断读取video_list, 播放最新视频。如果没有视频，就播放train.mp4的第一帧。
+    """
+    global video_list
+    video_path = None
+    audio_path = None
+    ret = None
+    frame = None
+    try:
+        while True:
+            # 找到要播放什么
+            is_not_idle = (len(video_list) > 0)
+            if is_not_idle:
+                video_path = video_list[0].get("video")
+                audio_path = video_list[0].get("audio")
+                cap = cv2.VideoCapture(video_path)  # 打开视频文件
+                video_list.pop(0)
+            else:
+                audio_path = None
+                # cap = None
+                # _, frame = cv2.VideoCapture(r"E:/AsciiStandardPath/InnovativeExploration/CFAR/xuniren/data/pretrained/train.mp4").read()
+                # _, frame = cv2.VideoCapture("results/ff384bb2-a890-4002-91dc-1fb581893b83/girl_face##sustcutech_10.mp4").read()
+                # frame = cv2.imread(face_path)
+                # frame = cv2.resize(frame, (256, 256))
+
+                cap = cv2.VideoCapture(待机动画)  
+
+            # 后台播放音频
+            if audio_path:
+                threading.Thread(target=play_audio, args=[audio_path]).start()  # play audio
+            # 循环播放视频帧
+            while True:
+                if not is_not_idle and (len(video_list) > 0):
+                    break # 跳出待机动画的循环
+                if cap:
+                    ret, frame = cap.read()
+                if frame is not None:  # 没有传音频过来时显示train.mp4的第一帧，建议替换成大约1秒左右的视频
+                    cv2.imshow("SadTalker", frame)
+                    # 等待 38 毫秒
+                    cv2.waitKey(38) # 帧率控制
+                if not ret:
+                    break
+    except InterruptedError as e:
+        print(f"play_video interrupted: {e}")
+    finally:
+        cv2.destroyAllWindows()
+
+
+
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Setup code
+    threading.Thread(target=play_video, 
+                          daemon=True).start()
+    
+    def play_background():
+        while True:
+            for bgm in 直播室背景音乐:
+                print("播放背景音乐：", bgm)
+                play_audio(bgm, 
+                        #    volume=0.1
+                           volume=0.05
+                           )
+                print("播放完毕")
+                time.sleep(1)
+    threading.Thread(target=play_background,  
+                     daemon=True).start()
+    
+    print("FastAPI application is starting. ")
+    yield
+    print("FastAPI application is shutting down. ")
+
+    # Teardown code
+    # t1.join() # daemon=True 那就不用join了
     
 # 2. FastAPI 有关功能
-app = FastAPI()
+# https://fastapi.tiangolo.com/advanced/events/#alternative-events-deprecated
+app = FastAPI(lifespan=lifespan)
 def start_fastapi_server():
-    uvicorn.run(app, host="0.0.0.0", port=18800)
+    uvicorn.run(app, host="127.0.0.1", port=18800, 
+                )
+
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+executor = ThreadPoolExecutor(max_workers=2)
+# asyncio.get_event_loop().set_default_executor(executor)
+# app.state.executor = executor
     
 # 3. 全局状态
 video_list: list[
@@ -127,7 +246,11 @@ video_list: list[
 video_cache: dict[str, str] = {}  # 是 {"audio_hash": "video_path"} 的字典，根据md5推算出来的视频在哪里
 
 @app.get("/audio_to_video/")
-async def audio_to_video(file_path: str):
+async def audio_to_video_async(file_path: str):
+    return await asyncio.get_event_loop().run_in_executor(executor, audio_to_video, (file_path, ))
+
+def audio_to_video(file_path: str):
+    
     print("file_path=", file_path)
     aud_dir = file_path
     aud_dir = aud_dir.replace("\\", "/")
@@ -152,106 +275,68 @@ async def audio_to_video(file_path: str):
         # audio_path_eo = "data/audio/aud_%d_eo.npy" % num
         # video_path = "data/video/results/ngp_%d.mp4" % num
         output_path = "data/video/results/output_%d.mp4" % num
-
+        Path(output_path).parent.mkdir(parents=True, exist_ok=True)
         
         # generate_video(audio_path, audio_path_eo, video_path, output_path)
+        # output_path = await generate_video(audio_path, output_path)
         output_path = generate_video(audio_path, output_path)
     
-        
-
         video_list.append({"video": output_path, "audio": new_path})
         video_cache[audio_hash] = output_path
 
     return {"code": 200}
 
 
+if __name__ == "__main__":
+    start_fastapi_server()
+    
+    
 
 
+# import threading, time, signal, sys
 
-def play_video():
-    """不断读取video_list, 播放最新视频。如果没有视频，就播放train.mp4的第一帧。
-    """
-    global video_list
-    video_path = None
-    audio_path = None
-    ret = None
-    frame = None
-    while threading.current_thread().stop_flag:
-        # 找到要播放什么
-        if len(video_list) > 0:
-            video_path = video_list[0].get("video")
-            audio_path = video_list[0].get("audio")
-            cap = cv2.VideoCapture(video_path)  # 打开视频文件
-            video_list.pop(0)
-        else:
-            audio_path = None
-            cap = None
-            # _, frame = cv2.VideoCapture(r"E:/AsciiStandardPath/InnovativeExploration/CFAR/xuniren/data/pretrained/train.mp4").read()
-            _, frame = cv2.VideoCapture("results/ff384bb2-a890-4002-91dc-1fb581893b83/girl_face##sustcutech_10.mp4").read()
-            # frame = cv2.imread(face_path)
-            # frame = cv2.resize(frame, (256, 256))
-
-        # 后台播放音频
-        if audio_path:
-            threading.Thread(target=play_audio, args=[audio_path]).start()  # play audio
-        # 循环播放视频帧
-        while threading.current_thread().stop_flag:
-            if cap:
-                ret, frame = cap.read()
-            if frame is not None:  # 没有传音频过来时显示train.mp4的第一帧，建议替换成大约1秒左右的视频
-                cv2.imshow("Fay-2d", frame)
-                # 等待 38 毫秒
-                cv2.waitKey(38) # 帧率控制
-            if not ret:
-                break
-    cv2.destroyAllWindows()
-
-
-
-import threading, time, signal, sys
-
-class StoppableThread(threading.Thread):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.stop_flag = False
-        self.daemon = True
-    def stop(self):
-        self.stop_flag = True
+# class StoppableThread(threading.Thread):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self.stop_flag = False
+#         self.daemon = True
+#     def stop(self):
+#         self.stop_flag = True
 # StoppableThread = threading.Thread
 # StoppableThread.stop = threading.Thread._stop
 # StoppableThread.stop_flag = threading.Thread._is_stopped
 
-@app.on_event("shutdown")
-def shutdown_event():
-    # 在这里添加你的清理逻辑
-    print("FastAPI application is shutting down。 ")
-def signal_handler(signal, frame):
-    print(f"Caught signal {signal} in frame {frame}.")
-    for thread in threading.enumerate():
-        # if isinstance(thread, StoppableThread):
-            thread._stop()
-    print("All threads are stopped. ")
-    sys.exit(0)
-    print("sys.exit(0) is called. ")
+# @app.on_event("shutdown")
+# def shutdown_event():
+#     # 在这里添加你的清理逻辑
+#     print("FastAPI application is shutting down。 ")
+# def signal_handler(signal, frame):
+#     print(f"Caught signal {signal} in frame {frame}.")
+#     for thread in threading.enumerate():
+#         # if isinstance(thread, StoppableThread):
+#             thread._stop()
+#     print("All threads are stopped. ")
+#     sys.exit(0)
+#     print("sys.exit(0) is called. ")
     
-if __name__ == "__main__":
-    # 创建并启动FastAPI服务的线程
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.signal(signal.SIGTERM, signal_handler)
-    threads:list[StoppableThread] = [
-        StoppableThread(target = start_fastapi_server),
-        StoppableThread(target = play_video)
-    ]
-    for thread in threads:
-        thread.start()
-    try:
-        for thread in threads:
-            thread.join()
-    except KeyboardInterrupt as e:
-        # 这个优先级没有signal_handler高，所以应该不会被调用
-        print(f"Caught KeyboardInterrupt {e}. ")
-        for thread in threads:
-            thread.stop()
+# if __name__ == "__main__":
+#     # 创建并启动FastAPI服务的线程
+#     signal.signal(signal.SIGINT, signal_handler)
+#     signal.signal(signal.SIGTERM, signal_handler)
+#     threads:list[StoppableThread] = [
+#         StoppableThread(target = start_fastapi_server),
+#         StoppableThread(target = play_video)
+#     ]
+#     for thread in threads:
+#         thread.start()
+#     try:
+#         for thread in threads:
+#             thread.join()
+#     except KeyboardInterrupt as e:
+#         # 这个优先级没有signal_handler高，所以应该不会被调用
+#         print(f"Caught KeyboardInterrupt {e}. ")
+#         for thread in threads:
+#             thread.stop()
             
     # daemon的优先级也不高，整个退出的时候才会被调用
     
